b39e6d34866f3e9ed296dfeab29040aa2251bcd1
BraisMorgade <braismor@ucm.es>
2021-01-22 20:51:02 +0100

    comentarios

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -3,10 +3,12 @@ using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public static class SaveSystem{
+    //Guarda el progreso en un fichero json
     public static void SaveProgress(PlayerProgress progress)
     {
         string json = JsonUtility.ToJson(progress);
 
+        //Genera el hash asignado al json que hemos creado y lo guarda
         string hashValue = SecureHelper.Hash(json);
         PlayerPrefs.SetString("HASH", hashValue);
 
@@ -22,6 +24,7 @@ public static class SaveSystem{
 
     public static PlayerProgress LoadProgress()
     {
+        //crea un progreso de 0 en caso de que los datos de guardado hayan sido cambiados desde fuera del juego
         PlayerProgress progress = new PlayerProgress(0, 0, 0);
         string path = Application.persistentDataPath + "/progress.txt";
         if (File.Exists(path))
@@ -31,8 +34,9 @@ public static class SaveSystem{
                 string defaultValue = "HASH_NOT_GENERATED";
                 string json = reader.ReadToEnd();
                 string hashValue = SecureHelper.Hash(json);
+                //cargamos el hash que teníamos guardado
                 string hashStored = PlayerPrefs.GetString("HASH", defaultValue);
-
+                //comprueba que el hash del fichero de guardado sea el mismo que el último hash que hemos guardado
                 if(hashStored == hashValue || hashStored == defaultValue)
                 {
                     JsonUtility.FromJsonOverwrite(json, progress);
